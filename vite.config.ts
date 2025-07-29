0a1485ae8b77cc70d9038bcad9409c74c9dc0ac4
Streltsov Ivan <principdereva@gmail.com>
2025-07-29 16:57:42 +0200

    fix: distr building

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 
 export default defineConfig({
   assetsInclude: ["**/*.hbs"],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
 });
