90d0737f633ba49c121ed79dd8a077459b83dc9d
Streltsov Ivan <principdereva@gmail.com>
2025-07-29 16:58:19 +0200

    feat: 404, 500, some improvemets to styling

--- src/utils/renderComponent.ts
+++ src/utils/renderComponent.ts
@@ -4,22 +4,22 @@ import { getTemplate } from "./getTemplate";
 export async function renderComponent(
   name: string,
   templates: Record<string, string>,
-  data: any,
-  styles?: Record<string, string>
+  data: any
+  // styles?: Record<string, string>
 ): Promise<string> {
-  if (styles) {
-    const styleName = `/${name}/${name}.scss`;
+  // if (styles) {
+  //   const styleName = `/${name}/${name}.scss`;
 
-    const styleKey = Object.keys(styles).find((path) =>
-      path.includes(styleName)
-    );
+  //   const styleKey = Object.keys(styles).find((path) =>
+  //     path.includes(styleName)
+  //   );
 
-    if (styleKey) {
-      await import(`../${styleKey}`);
-    } else {
-      console.warn(`⚠️ No style found for ${name}`);
-    }
-  }
+  //   if (styleKey) {
+  //     await import(`../${styleKey}`);
+  //   } else {
+  //     console.warn(`⚠️ No style found for ${name}`);
+  //   }
+  // }
   const templateString = getTemplate(name, templates);
   const compiled = Handlebars.compile(templateString);
   return compiled(data);
