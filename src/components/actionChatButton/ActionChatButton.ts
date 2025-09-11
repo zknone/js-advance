48355d41fab916c988c08e3891ed0ae7151c9460
Streltsov Ivan <principdereva@gmail.com>
2025-09-11 19:44:28 +0200

    feat: action button

---
+++ src/components/actionChatButton/ActionChatButton.ts
@@ -0,0 +1,36 @@
+import CustomButton from '../customButton/CustomButton';
+import type { CustomButtonProps } from '../../types/chat';
+
+export type ActionType = 'add' | 'delete';
+
+interface ActionChatButtonProps extends Omit<CustomButtonProps, 'icon' | 'color'> {
+  action: ActionType;
+}
+
+class ActionChatButton extends CustomButton {
+  constructor(props: ActionChatButtonProps) {
+    const icons = {
+      add: {
+        src: '/public/add-cross.svg',
+        alt: 'Добавить пользователя',
+        width: 16,
+        height: 16,
+      },
+      delete: {
+        src: '/public/delete-cross.svg',
+        alt: 'Удалить пользователя',
+        width: 16,
+        height: 16,
+      },
+    };
+
+    super({
+      ...props,
+      icon: icons[props.action],
+      type: 'button',
+      text: props.action === 'add' ? 'Добавить' : 'Удалить',
+    });
+  }
+}
+
+export default ActionChatButton;
